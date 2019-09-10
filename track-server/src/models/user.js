cd0f8fdb270c5466bbe348ccee1ae26348111954
kareem sakr <kareem.h.sakr@gmail.com>
2019-09-10 15:55:13 -0400

    implemented sign up route and updated user schema with salting and hashing new users

--- track-server/src/models/user.js
+++ track-server/src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+var bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
   email: {
@@ -11,5 +12,40 @@ const userSchema = mongoose.Schema({
     required: true
   }
 });
+
+userSchema.pre("save", function(next) {
+  // only hash the password if it has been modified (or is new)
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const self = this;
+  // generate a salt
+  return bcrypt.genSalt(10, function(error, salt) {
+    if (error) {
+      return next(error);
+    }
+    console.log(this);
+    // hash the password using the new salt
+    return bcrypt.hash(self.password, salt, function(error, hash) {
+      if (error) {
+        return next(error);
+      }
+      // override the cleartext password with the hashed one
+      self.password = hash;
+      console.log(this);
+
+      return next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(passw, cb) {
+  bcrypt.compare(passw, this.password, function(err, isMatch) {
+    if (err) {
+      return cb(err, false);
+    }
+    return cb(null, isMatch);
+  });
+};
 
 mongoose.model("User", userSchema);
--- track-server/src/routes/auth.js
+++ track-server/src/routes/auth.js
@@ -5,8 +5,23 @@ const User = mongoose.model("User");
 
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
-  res.send("help");
+router.post("/signup", async (req, res) => {
+  try {
+    let newUser = new User({
+      email: req.body.email,
+      password: req.body.password
+    });
+    await newUser.save();
+    const token = "tempToken";
+    return res.status(201).json({
+      user: newUser,
+      token: `JWT ${token}`,
+      msg: "User was succesfully created"
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
 
 module.exports = router;
