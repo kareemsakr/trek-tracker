6c2b9fda3fde142e95498410e72f3645a168f718
kareem sakr <kareem.h.sakr@gmail.com>
2019-09-11 14:06:07 -0400

    updated sign up screen

--- track-client/screens/SignupScreen.js
+++ track-client/screens/SignupScreen.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../components/spacer";
 
 export default function SignUp({ navigation }) {
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
   return (
     <View style={styles.container}>
-      <Text h3>Sign Up</Text>
-      <Spacer />
       <Spacer>
-        <Input label="Email" />
-        <Spacer />
-        <Input label="Password" />
+        <Text h3>Sign Up</Text>
       </Spacer>
-      <Spacer />
-      <Button title="Sign Up" />
+      <Spacer>
+        <Input
+          label="Email"
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Spacer />
+        <Input
+          label="Password"
+          value={password}
+          onChangeText={setPassword}
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry
+        />
+      </Spacer>
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
     </View>
   );
 }
