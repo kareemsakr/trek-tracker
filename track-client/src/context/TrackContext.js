f3d1326b903573dd7c99181d6389876211a6c32d
kareem sakr <kareem.h.sakr@gmail.com>
2019-09-19 19:31:03 -0400

    updated track reducer

--- track-client/src/context/TrackContext.js
+++ track-client/src/context/TrackContext.js
@@ -4,6 +4,8 @@ import trackAPI from "../api/tracker";
 
 const reducer = (state, action) => {
   switch (action.type) {
+    case "add_tracks":
+      return [...action.payload];
     default:
       return state;
   }
